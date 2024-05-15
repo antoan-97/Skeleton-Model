3813aed48c3c6ea0286f2b606cc189d2bf42881f
antoan-97 <antoan.todorov@abv.bg>
2024-05-15 15:05:06 +0300

    validate if user already exists

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,6 +1,12 @@
 const User = require('../models/User');
 
-exports.register = (userData) => User.create(userData);
+exports.register = async (userData) => { const user = await User.findOne({
+    username:userData.username});
+    if(user){
+        throw new error('Username already exists');
+    }
+    return User.creaete(userData);
+    };
 
 // exports.login = async (username,password) => { const user = await User.findOne({username})};
 // if(!user){
