4a179d90b28316138baf36b7b995bd6975dc39f4
antoan-97 <antoan.todorov@abv.bg>
2024-05-15 14:58:22 +0300

    add login and register post methods

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
+const userManager = require('../managers/userManager');
 
-router.get('/login', (req,res) =>{
+router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
-router.get('/register', (req, res) =>{
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    await userManager.login(username, password);
+    res.send('Logged in')
+})
+
+router.get('/register', (req, res) => {
     res.render('users/register');
 });
+
+router.post('/register', async (req, res) => {
+    const { username, email, password, repeatPassword } = req.body;
+    await userManager.register({ username, email, password, repeatPassword });
+    res.send('registered')
+})
 
 module.exports = router
