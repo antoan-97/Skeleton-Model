a3f2bb0fb2b809bc9b0bab22ed2ae0304c227e34
antoan-97 <antoan.todorov@abv.bg>
2024-05-15 12:39:25 +0300

    add and config express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const routes = require('./routes');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
-app.get('/', (req,res) => {
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
     res.send('Action')
 })
 
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
-app.listen(5000,console.log('Server is listeningon port 5000'));
+
+app.listen(5000, console.log('Server is listeningon port 5000'));
