660b14fb23d7b86cec2c826d0b487d91eff9ba17
antoan-97 <antoan.todorov@abv.bg>
2024-05-16 12:37:49 +0300

    config cookie-parser

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const routes = require('./routes');
 const handlebars = require('express-handlebars');
 const path = require('path');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const { log } = require('console');
 
 const app = express();
@@ -21,6 +22,7 @@ app.set('views', 'src/views');
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}));
 app.use(routes);
+app.use(cookieParser);
 
 
 app.listen(5000, console.log('Server is listeningon port 5000'));
