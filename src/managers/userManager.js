70651874c0e86d4b1c6fabf0323461448391d451
antoan-97 <antoan.todorov@abv.bg>
2024-05-16 12:30:52 +0300

    generate token in manager login

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,23 +1,35 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
 
-exports.register = async (userData) => { 
-    const user = await User.findOne({username:userData.username});
-    if(user){
+const SECRET = 'somebigsecret';
+
+exports.register = async (userData) => {
+    const user = await User.findOne({ username: userData.username });
+    if (user) {
         throw new error('Username already exists');
     }
     return User.create(userData);
+};
+
+exports.login = async (username, password) => {
+    const user = await User.findOne({ username });
+    if (!user) {
+        throw new Error('Invaid username or password!')
+    }
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        throw new Error('Invalid username or password!');
+    }
+
+    const payload = {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
     };
 
-exports.login = async (username,password) => { 
-const user = await User.findOne({username});
-if(!user) {
-    throw new Error('Invaid username or password!')
-}
-const isValid = await bcrypt.compare(password, user.password);
-if(!isValid){
-    throw new Error('Invalid username or password!');
-}
+    const token = await jwt.sign(payload, SECRET, { expiresIn: "2d" });
+    return token;
 
 }
 
