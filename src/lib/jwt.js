ec7f8d901e1daaec0d1fe99ec9457994eb2b6448
antoan-97 <antoan.todorov@abv.bg>
2024-05-16 12:37:58 +0300

    fix syntax error

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -1,10 +1,9 @@
-const { sign, verify } = require('crypto');
 const jsonwebtoken = require('jsonwebtoken');
 const util = require('util');
 
 const jwt = {
-    sign:util.promosify(jsonwebtoken.sign),
-    verify:util.promisify(jsonwebtoken.verify)
+    sign: util.promisify(jsonwebtoken.sign),
+    verify: util.promisify(jsonwebtoken.verify)
 };
 
 module.exports = jwt;
